c301fa7e0710e8debe5adcfaa8f9e937bbb2a9cd
Anthony Ventimiglia <ajventi@users.noreply.github.com>
2018-10-23 09:04:53 -0400

    Working on the first function to help with cellar work

---
+++ racking-entry.ps1
@@ -0,0 +1,127 @@
+class RackingEntry {
+    [System.Data.Odbc.OdbcTransaction] $transaction
+    [System.Collections.Hashtable]$blendComponents
+    [string]$date
+    [int] hidden $_blendId
+    [int] BlendID () {
+        return $this._blendId
+    }
+    [int] makeNewBlend ([string] $name, [string] $color, [int] $year) {
+        if ($this._blendId -ne 0) { return $this._blendId }
+        if ($this.blendComponents.Count -eq 1) {
+            $k = @(0)
+            $this.blendComponents.Keys.CopyTo($k,0)
+            $this._blendId = $k[0]
+        } else {
+            $cmd = $this.transaction.Connection.CreateCommand()
+            $cmd.Transaction = $this.transaction
+            $cmd.CommandText = "INSERT INTO BLEND (year, name, color) SELECT"
+            $cmd.CommandText += " $year, '$name', '$color' RETURNING blend_id;"
+            $rdr = $cmd.ExecuteReader()
+            $this._blendId = $rdr.GetInt32(0)
+            $this.blendComponents.keys | ForEach-Object {
+                $cmd.CommandText = 'INSERT INTO blend_component (blend_id, component_blend_id, volume, date)'
+                $cmd.CommandText += 'SELECT $this._blendId, $_, $this.blendComponents.$_, $date;'
+                $cmd.ExecuteNonQuery()
+            }
+            $rdr.Close()
+            $cmd.Dispose()
+        }
+        return $this._blendId
+    }
+}
+
+function ConvertTo-SQL {
+    Param (
+        [int[]]$TextList
+    )
+    $str = ConvertTo-Json $TextList -Compress
+    return $str.Replace("[","(").Replace("]",")")
+}
+function New-RackingEntry {
+    <#  
+    .SYNOPSIS 
+        Prepare entry to record racking and blending cellar activity.
+    .DESCRIPTION
+        Records all wines emptied into a blend, updates database inside a transaction.
+
+        Returns WineBlob Object, which will then be used to record the filling of bulk 
+        containers by the Set-RackingEntry commandlet.
+
+        Please write more help
+    #>
+    [CmdletBinding()]
+    [OutputType([RackingEntry])]
+    Param (
+        #OdbcConnection (Must first be open)
+        [Parameter(Mandatory=$true)]
+        [System.Data.Odbc.OdbcConnection]
+        $OdbcConnection,
+
+        #Bulk IDs of all emptied bulk containers
+        [Parameter(Mandatory=$true)]
+        [int[]] 
+        $SupplyIDs,
+        
+        #Date of activity, defaults to CURRENT_DATE
+        [Parameter(Mandatory=$false)]
+        [string] 
+        $Date = "CURRENT_DATE",
+
+        # Echo SQL Commands, do not actually perform anything 
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $simulate
+
+    )
+    $transaction = $OdbcConnection.BeginTransaction()
+    $cmd = $OdbcConnection.CreateCommand()
+    $cmd.Transaction = $transaction
+    $ids = ConvertTo-SQL($SupplyIDs)
+    $cmd.CommandText = "UPDATE bulk_wine SET EMPTY_DATE = $Date"
+    $cmd.CommandText += " WHERE id IN $ids RETURNING id, blend_id, volume;"
+    if ($simulate -eq $true) {
+        Write-Output $cmd.CommandText
+    } else {
+        $reader = $cmd.ExecuteReader()
+    }
+    $blendComponents = @{}
+    while ($reader.read()) {
+        $blendComponents[$reader.getInt32(1)] += $reader.GetDouble(2)
+    }
+    
+    # What needs to be in our RackingEntry Object?
+    $r = New-Object RackingEntry
+    $r.transaction = $transaction
+    $r.blendComponents = $blendComponents
+    $r.date = $date
+    $reader.Close()
+    $cmd.Dispose()
+
+    return $r
+}
+
+function Set-RackingEntry {
+    <#
+    .SYNOPSIS
+        Finish Entry of cellar racking and blending activity.
+    .DESCRIPTION
+        After Emptying wines, and ensuring a valid blend_id, record the filling of new 
+        bulk_containers.
+
+        Returns list of bulk_ids for new entries
+    #>
+}
+
+function Show-RackingEntry {
+    <#
+    .SYNOPSIS 
+        Show database inside transaction to validate.
+    #>
+}
+# Hey wait, seems like the RackingEntry class should be doing all of this 
+function Close-RackingEntry {
+
+}
+
+
