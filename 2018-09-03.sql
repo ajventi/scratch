8d4c73061ebf4bac42214c7c8ca3a473e2bdd1ff
Anthony Ventimiglia <ajventi@users.noreply.github.com>
2018-10-24 08:12:01 -0400

    Energize

--- 2018-09-03.sql
+++ 2018-09-03.sql
@@ -1,3 +1,5 @@
+-- Original SQL Code Has been inserted for reals
+
 UPDATE Bulk_wine set empty_date = '2018-09-03' where id = 4014100 returning *;
 INSERT INTO Bulk_Wine (fill_date, container_id, blend_id, volume )  SELECT '2018-09-03', 'VT-1k5', 2014032, 1000;
 SELECT * FROM Bulk_inventory where container_id = 'VT-1k5';
